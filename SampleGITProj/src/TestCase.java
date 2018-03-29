ba5267f95ca2d1bdeebaeb40be7bcb097681b6b0
RamaMannepuri <37792897+RamaMannepuri@users.noreply.github.com>
2018-03-29 16:11:07 +0530

    Added some more names from the remote GitHub

--- SampleGITProj/src/TestCase.java
+++ SampleGITProj/src/TestCase.java
@@ -2,7 +2,7 @@
 public class TestCase {
 
 	public static void main(String[] args) {
-		String names[] = {"Murali", "Rama", "Prajjwal", "Shivansh"};
+		String names[] = {"Murali", "Rama", "Prajjwal", "Shivansh", "Prabhavathi", "Satyanarayana", "Prasanna"};
 		for(String name : names){
 			System.out.println(name);
 		}
