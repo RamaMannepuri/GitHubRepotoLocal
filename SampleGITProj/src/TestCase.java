03dc5b555c0be7c4038a1bf5ca9290ebae883686
Ramalakshmi <ramalakshmi.eerpina@nihilent.com>
2018-03-29 16:06:14 +0530

    First commit from the Local Repository

---
+++ SampleGITProj/src/TestCase.java
@@ -0,0 +1,12 @@
+
+public class TestCase {
+
+	public static void main(String[] args) {
+		String names[] = {"Murali", "Rama", "Prajjwal", "Shivansh"};
+		for(String name : names){
+			System.out.println(name);
+		}
+
+	}
+
+}
